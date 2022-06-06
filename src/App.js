500b6120ed1c95930a87dc472e1af8b72d8c53cc
Agata Dopierała <agatadopierala@vaimo.com>
2022-06-06 14:10:17 +0100

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import { HeadProvider, Title } from 'react-head';
+// import Main from '';
+// import Mask from 'react-input-mask';
+// import Routes from '@magento/venia-ui/lib/components/Routes';
+// import useLoadingBar from 'talons/LoadingBar';
+// import globalCSS from 'style/base/index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+    const { markErrorHandled, renderError, unhandledErrors } = props;
+
+    return (
+        <HeadProvider>
+            <Title>Homepage</Title>
+            {/*<Main>*/}
+            {/*    <Routes />*/}
+            {/*</Main>*/}
+            {/*<Mask isActive={hasOverlay} dismiss={handleCloseDrawer} />*/}
+        </HeadProvider>
+    );
+};
+
+App.propTypes = {};
+
+App.displayName = 'App';
+
+App.globalCSS = globalCSS;
 
 export default App;
