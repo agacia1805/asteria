b04d98a372bb7bab0902fa98636b36ff6abb9a81
Agata Dopierała <agatadopierala@vaimo.com>
2022-06-13 19:01:25 +0100

    Change the name of the app and add icons

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,18 +1,8 @@
 import React from 'react';
-// import { Provider as ReduxProvider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const Adapter = () => {
-    return (
-        // <ReduxProvider>
-        <BrowserRouter>
-            {/*<AppContextProvider>*/}
-            <App />
-            {/*</AppContextProvider>*/}
-        </BrowserRouter>
-        // </ReduxProvider>
-    );
+    return <App />;
 };
 
 export default Adapter;
