18044a02ada00cd6bac0847e367f216d040c7bc3
Agata Dopierała <agatadopierala@vaimo.com>
2022-06-28 21:18:02 +0100

    Update container component

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import classes from 'classnames/bind';
 import defaultClasses from './Container.scss';
+
+const cx = classes.bind({ ...defaultClasses });
 
 const Container = (children, className, variant) => {
     const classes = cx(className, defaultClasses.root, {
